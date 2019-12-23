03e7b5e825827e29b96a8c638b213700e972f479
Rostyslav Yatsura <floxaz1@gmail.com>
2019-12-23 13:15:59 +0100

    implemented authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { OffersComponent } from './offers/offers.component';
 import { AuthComponent } from './auth/auth.component';
 import { CartComponent } from './cart/cart.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'offers', component: OffersComponent},
-  {path: 'cart', component: CartComponent},
+  {path: 'offers', component: OffersComponent, canActivate: [AuthGuardService]},
+  {path: 'cart', component: CartComponent, canActivate: [AuthGuardService]},
   {path: 'login', component: AuthComponent},
   {path: 'signup', component: AuthComponent},
   {path: 'not-found', component: PageNotFoundComponent},
