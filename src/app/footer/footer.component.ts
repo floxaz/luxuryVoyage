73a14db32e471a31e69ac3a46be4556a891a65e8
Rostyslav Yatsura <floxaz1@gmail.com>
2020-01-11 17:43:08 +0100

    changed footer view on home component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  isHome: boolean;
   year = new Date().getFullYear();
   language = 'it';
 
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService, private router: Router) { }
 
   ngOnInit() {
     if (this.translate.currentLang) {
@@ -18,6 +20,16 @@ export class FooterComponent implements OnInit {
     } else {
       this.language = this.translate.getDefaultLang();
     }
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url !== '/') {
+          this.isHome = false;
+        } else {
+          this.isHome = true;
+        }
+      }
+    });
   }
 
   changeLanguage() {
