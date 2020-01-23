fbbee5882414f78ea56d286fadabb5df5021d513
Rostyslav Yatsura <floxaz1@gmail.com>
2020-01-23 22:02:26 +0100

    added repeat password control on signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -15,17 +15,54 @@ export class AuthComponent implements OnInit {
   isLogin = true;
   isLoading = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
-    if (this.router.url === '/signup') {
-      this.isLogin = false;
-    }
-
     this.authForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.pattern(/^[0-9a-zA-Z,. !]{6,}/)])
     });
+
+    if (this.router.url === '/signup') {
+      this.isLogin = false;
+
+      this.authForm.get('password').setValidators([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^[0-9a-zA-Z,. !]/),
+        this.compareToRepeatPassword.bind(this)
+      ]);
+      this.authForm.addControl('repeatPassword', new FormControl(null, [
+        Validators.required,
+        this.comparePassword.bind(this)
+      ]));
+
+    } else {
+      this.authForm.get('password').setValidators([
+        Validators.required,
+        Validators.pattern(/^[0-9a-zA-Z,. !]/),
+      ]);
+    }
+  }
+
+  compareToRepeatPassword(control: FormControl) {
+    if (this.authForm.get('repeatPassword').value && control.value !== this.authForm.get('repeatPassword').value) {
+      this.authForm.get('repeatPassword').setErrors({
+        invalidPassword: true
+      });
+    } else {
+      this.authForm.get('repeatPassword').setErrors({
+        invalidPassword: null
+      });
+    }
+  }
+
+  comparePassword(control: FormControl) {
+    if (control.value !== this.authForm.get('password').value) {
+      return {
+        invalidPassword: true
+      };
+    }
   }
 
   onSubmit() {
