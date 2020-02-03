f54d3516fe5c61b3387e0269f4c1e725d9cfcdb4
Rostyslav Yatsura <floxaz1@gmail.com>
2020-02-03 20:46:44 +0100

    adjusted server response

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy, that allows Angular to handle routing instead of the server
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/luxuryvoyage/index.html'));
 });
