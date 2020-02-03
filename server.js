12bf50632b14fc96e3e6b2b27b9b8c3824bc9533
Rostyslav Yatsura <floxaz1@gmail.com>
2020-02-03 20:29:06 +0100

    Setup production build and server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+// PathLocationStrategy, that allows Angular to handle routing instead of the server
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
