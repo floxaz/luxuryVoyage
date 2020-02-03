610cd7da67006a13ce30f9f23d4d54e41dd7a548
Rostyslav Yatsura <floxaz1@gmail.com>
2020-02-03 20:55:12 +0100

    adjusted server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/luxuryvoyage'));
 
-app.listen(process.env.PORT || 8080);
-
-// PathLocationStrategy, that allows Angular to handle routing instead of the server
-app.get('/*', (req, res) => {
+// Send all requests to index.html
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/luxuryvoyage/index.html'));
 });
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
